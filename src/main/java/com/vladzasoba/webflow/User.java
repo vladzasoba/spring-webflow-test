54d79738f8ee54750d62bb284163945c7460ba51
Vladyslav Zasoba <vladzasoba16@gmail.com>
2018-05-14 07:52:36 +0300

    Improve User model class

--- src/main/java/com/vladzasoba/webflow/User.java
+++ src/main/java/com/vladzasoba/webflow/User.java
@@ -8,6 +8,13 @@ public class User implements Serializable {
 	private String username;
 	private String password;
 
+	public User() {}
+
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
 	public String getUsername() {
 		return username;
 	}
@@ -22,5 +29,29 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = result * prime + ((username == null) ? 0 : username.hashCode());
+		result = result * prime + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (obj.getClass() != getClass()) {
+			return false;
+		}
+		User user = (User) obj;
+		return username.equals(user.getUsername()) && password.equals(user.getPassword());
 	}
 }
--- src/main/java/com/vladzasoba/webflow/UserService.java
+++ src/main/java/com/vladzasoba/webflow/UserService.java
@@ -1,9 +1,13 @@
 package com.vladzasoba.webflow;
 
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.ArrayList;
 
 @Service
 public class UserService {
+	private List<User> userList = new ArrayList<>();
+
 	public String checkUser(User user) {
 		if (user.getUsername() != null && user.getUsername().equals("admin")
 				&& user.getPassword() != null && user.getPassword().equals("1111aaaa")) {
